2a851917da4ec9da6f0da161f48149e41d1f0eac
MagamadovM <mobilnik9009@gmail.com>
2024-09-11 18:07:01 +0300

    fixed UserService & main

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private UserDao userDao = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
         userDao.createUsersTable();
 
@@ -33,5 +36,4 @@ public class UserServiceImpl implements UserService {
         userDao.cleanUsersTable();
 
     }
-    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
 }
